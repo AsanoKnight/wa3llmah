3a827f690cf703fa3692ed1da89515e653a85475
wjeeh <wjeehs50@gmail.com>
2020-03-09 23:50:19 +0300

    put sout

--- JavaFX/AdvancProgramming/src/advancprogramming/AdvancProgramming.java
+++ JavaFX/AdvancProgramming/src/advancprogramming/AdvancProgramming.java
@@ -20,7 +20,7 @@ public class AdvancProgramming extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        System.out.println("fuck you");
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
