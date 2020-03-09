4c0aeeaf6225fa233ae0cbeaa623a0a6dbbd533a
maherapi <maher.azkoul@gmail.com>
2020-03-09 23:52:44 +0300

    fx: for expirement

--- JavaFX/AdvancProgramming/src/advancprogramming/AdvancProgramming.java
+++ JavaFX/AdvancProgramming/src/advancprogramming/AdvancProgramming.java
@@ -20,6 +20,7 @@ public class AdvancProgramming extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        System.out.println("from maher");
         
         Scene scene = new Scene(root);
         
